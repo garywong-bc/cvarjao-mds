5301d04a4a357fe00c9f79e78a45b2977ece6ad4
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-07-17 19:29:48 -0700

    Pipeline

---
+++ Jenkinsfile
@@ -0,0 +1,59 @@
+pipeline {
+    agent none
+    stages {
+        stage('Build') {
+            agent { label 'master' }
+            steps {
+                echo "Building ..."
+            }
+        }
+        stage('Quality Control') {
+            agent { label 'master' }
+            steps {
+                echo "Quality Control ..."
+            }
+        }
+        stage('Deploy (DEV)') {
+            agent { label 'master' }
+            steps {
+                echo "Deploy (DEV) ..."
+            }
+        }
+        stage('Test (DEV)') {
+            agent { label 'master' }
+            steps {
+                echo "Test (DEV) ..."
+            }
+        }
+        stage('Deploy (TEST)') {
+            agent { label 'master' }
+            steps {
+                echo "Deploy (TEST)"
+            }
+        }
+        stage('Test (TEST)') {
+            agent { label 'master' }
+            steps {
+                echo "Test (TEST) ..."
+            }
+        }
+        stage('Deploy (PROD)') {
+            agent { label 'master' }
+            steps {
+                echo "Deploy (PROD)"
+            }
+        }
+        stage('Verify (PROD)') {
+            agent { label 'master' }
+            steps {
+                echo "Verify (PROD) ..."
+            }
+        }
+        stage('Acceptance') {
+            agent { label 'master' }
+            steps {
+                echo "Acceptance ..."
+            }
+        }
+    }
+}
---
+++ pipeline/config.groovy
@@ -0,0 +1,79 @@
+app {
+    name = 'mds'
+    version = 'snapshot'
+    environments = ["dev", "test", "prod"]
+
+    git {
+        workDir = ['git', 'rev-parse', '--show-toplevel'].execute().text.trim()
+        uri = ['git', 'config', '--get', 'remote.origin.url'].execute().text.trim()
+        commit = ['git', 'rev-parse', 'HEAD'].execute().text.trim()
+        ref = ['bash','-c', 'git config branch.`git name-rev --name-only HEAD`.merge'].execute().text.trim()
+        changeId = "${opt.'pr'}"
+    }
+
+    build {
+        name = "pr-${app.git.changeId}"
+        prefix = "${app.name}-"
+        suffix = "-${app.git.changeId}"
+        namespace = 'csnr-devops-lab-tools'
+        timeoutInSeconds = 60*20 // 20 minutes
+        templates = [
+                [
+                    'file':'openshift/_nodejs.bc.json',
+                    'params':[
+                        'NAME':"mds-web-api",
+                        'SUFFIX': "${app.build.suffix}",
+                        'OUTPUT_TAG_NAME':"pr-${app.git.changeId}",
+                        'SOURCE_CONTEXT_DIR': "web-api",
+                        'SOURCE_REPOSITORY_URL': "${app.git.uri}"
+                    ]
+                ],
+        ]
+    }
+
+    deployment {
+        name = "${vars.deployment.key}-pr-${app.git.changeId}"
+        prefix = "${app.name}-"
+        suffix = "-${app.git.changeId}"
+        namespace = "${vars.deployment.namespace}"
+        timeoutInSeconds = 60*20 // 20 minutes
+        templates = [
+                [
+                    'file':'openshift/_nodejs.dc.json',
+                    'params':[
+                        'NAME':"mds-web-api",
+                        'SUFFIX': "${app.deployment.suffix}",
+                        'TAG_NAME':"${app.deployment.name}",
+                        'APPLICATION_DOMAIN': "${vars.modules.'web-api'.HOST}"
+                    ]
+                ]
+        ]
+    }
+}
+
+//Default Values (Should it default to DEV or PROD???)
+vars {
+    modules {
+        'web-api' {
+            HOST = "mds-api-${app.git.changeId}-csnr-devops-lab-deploy.pathfinder.gov.bc.ca"
+        }
+    }
+}
+
+environments {
+    'dev' {
+        vars {
+            DB_PVC_SIZE = '1Gi'
+            deployment {
+                key = 'dev'
+                namespace = 'csnr-devops-lab-deploy'
+            }
+        }
+    }
+    'test' {
+
+    }
+    'prod' {
+
+    }
+}
