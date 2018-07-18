6b8e6eff1fa995049d0144ad25cfaaaa9b4ec371
Clecio Varjao <1348549+cvarjao@users.noreply.github.com>
2018-07-17 20:48:13 -0700

    Bumping pipeline version

--- pipeline/build.groovy
+++ pipeline/build.groovy
@@ -1,7 +1,7 @@
 import groovy.transform.BaseScript
 
 @GrabResolver(name='_pipeline', root='https://dl.bintray.com/cvarjao/maven')
-@Grab(group='ca.bc.gov.devops.cd.pipeline', module='ocp-pipeline', version='[0.0.3,0.1.0)')
+@Grab(group='ca.bc.gov.devops.cd.pipeline', module='ocp-pipeline', version='0.0.3')
 import ca.bc.gov.devops.OpenShiftBuildHelper
 
 @BaseScript ca.bc.gov.devops.BasicBuild _super
